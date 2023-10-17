320c880fcf267576777f06cc9113a991c43a0456
junaeddurjoy <junaed.durjoy@gmail.com>
2023-10-17 18:59:18 +0600

    client side dependency loaded

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl'>Brand Shop</h1>
+      <img className="mask mask-squircle" src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg" />
+    </>
+  )
+}
+
+export default App
