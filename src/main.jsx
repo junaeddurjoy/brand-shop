9f352624e6dae2824531c67e70f099690c1cbd33
junaeddurjoy <junaed.durjoy@gmail.com>
2023-10-19 18:05:47 +0600

    error page handled

--- src/main.jsx
+++ src/main.jsx
@@ -16,51 +16,62 @@ import SignUp from './pages/SignUp';
 import AuthProvider from './pages/provider/AuthProvider';
 import BrandProducts from './pages/BrandProducts';
 import PrivateRoutes from './PrivateRoutes';
-// import Brand from './pages/Brand';
+import ErrorPage from './pages/ErrorPage';
+import Root from './pages/Root';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
-    loader: () => fetch('http://localhost:5000/product')
-  },
-  {
-    path: "/addProduct",
-    element: <AddProduct></AddProduct>,
-  },
-  {
-    path: "/login",
-    element: <Login></Login>,
-  },
-  {
-    path: "/signup",
-    element: <SignUp></SignUp>,
-  },
-  {
-    path: "/cart",
-    element: <PrivateRoutes><Cart></Cart></PrivateRoutes>,
-    loader: () => fetch('http://localhost:5000/cart')
-  },
-  {
-    path: "/updateProduct/:id",
-    element: <UpdateProduct></UpdateProduct>,
-    loader: ({ params }) => fetch(`http://localhost:5000/product/${params.id}`)
-  },
-  {
-    path: "/products",
-    element: <Products></Products>,
-    loader: () => fetch('http://localhost:5000/product')
-  },
-  {
-    path: "/brandproducts/:brand",
-    element: <BrandProducts></BrandProducts>,
-    loader: ({params}) => fetch(`http://localhost:5000/brandproduct/${params.brand}`)
-  },
-  {
-    path: "/product/:id",
-    element: <ProductDetails></ProductDetails>,
-    loader: ({ params }) => fetch(`http://localhost:5000/product/${params.id}`),
-  },
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        errorElement: <ErrorPage></ErrorPage>,
+        loader: () => fetch('http://localhost:5000/product')
+      },
+      {
+        path: "/addProduct",
+        element: <AddProduct></AddProduct>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: "/cart",
+        element: <PrivateRoutes><Cart></Cart></PrivateRoutes>,
+        loader: () => fetch('http://localhost:5000/cart')
+      },
+      {
+        path: "/updateProduct/:id",
+        element: <UpdateProduct></UpdateProduct>,
+        loader: ({ params }) => fetch(`http://localhost:5000/product/${params.id}`)
+      },
+      {
+        path: "/products",
+        element: <Products></Products>,
+        loader: () => fetch('http://localhost:5000/product')
+      },
+      {
+        path: "/brandproducts/:brand",
+        element: <BrandProducts></BrandProducts>,
+        loader: ({params}) => fetch(`http://localhost:5000/brandproduct/${params.brand}`)
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetails></ProductDetails>,
+        loader: ({ params }) => fetch(`http://localhost:5000/product/${params.id}`),
+      },
+    ]
+  }
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/BrandProducts.jsx
+++ src/pages/BrandProducts.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData} from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import ProductCard from "./ProductCard";
@@ -7,15 +7,47 @@ import dummyData from '../../public/brands.json'
 const BrandProducts = () => {
     const products = useLoaderData();
     let bname = products[0].brand;
-    
+    let foundObject = dummyData.find(obj => obj.name === bname);
 
+    if (foundObject) {
+        console.log('hehhe mama paisi', foundObject);
+    }
+    const { image1, image2, image3, image4 } = foundObject;
     return (
         <div>
             <Navbar></Navbar>
             <div>
-                {
-                    
-                }
+                <h1 className="text-4xl font-bold font-pacifico text-center">{bname}</h1>
+                <div className="carousel h-[30rem] w-full">
+                    <div id="slide1" className="carousel-item relative w-full">
+                        <img src={image1} className="w-full" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide4" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide2" className="carousel-item relative w-full">
+                        <img src={image2} className="w-full" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide1" className="btn btn-circle">❮</a>
+                            <a href="#slide3" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide3" className="carousel-item relative w-full">
+                        <img src={image3} className="w-full" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide2" className="btn btn-circle">❮</a>
+                            <a href="#slide4" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide4" className="carousel-item relative w-full">
+                        <img src={image4} className="w-full" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide3" className="btn btn-circle">❮</a>
+                            <a href="#slide1" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                </div>
             </div>
             <h1 className="text-3xl mt-14 font-pacifico font-bold mx-10">Products:</h1>
             <div className="grid grid-cols-1 lg:grid-cols-4 mb-14 mt-7 mx-10">
